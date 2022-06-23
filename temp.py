e0e28f4cb0b524a802e3d3d84b1c9afca366eda1
rui1610 <rui.nogueira1610@hotmail.com>
2022-06-23 11:58:44 +0200

    remove trial tests

--- temp.py
+++
@@ -1,9 +0,0 @@
-import json
-
-filename = "trial_entitledServices.json"
-f = open(filename)
-# returns JSON object as a dictionary
-data = json.load(f)
-
-for item in data.get("entitledServices"):
-    print(item.get("displayName") + " ; " + item.get("description") )
